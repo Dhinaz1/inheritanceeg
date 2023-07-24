9ad6aa359cc58d366dd426690a0b247c4b4d881e
Dhinadhar <Dhinadhar@DESKTOP-MFT0AI4>
2023-07-24 15:32:06 +0530

    initial commit

---
+++ JAVAnew/src/testcase/LetterRepeater.java
@@ -0,0 +1,12 @@
+package testcase;
+import java.util.Scanner;
+public class LetterRepeater{
+	    public static void main(String[] args) {
+	        Scanner s = new Scanner(System.in);
+	        System.out.print("Input: ");
+	        String str = s.nextLine();
+	        int m,a;
+	        
+	        
+	    }
+}
